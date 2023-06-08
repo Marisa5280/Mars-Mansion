887f98251d7e0006f8e346db8b2de2589d58d262
Marisa Wyatt <risawyatt22@gmail.com>
2023-06-08 10:56:10 -0600

    feat: Add files and fetch

--- src/scripts.js
+++ src/scripts.js
@@ -7,5 +7,5 @@ import './css/styles.css';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-
+//promise.all (translate data from fetch to sources of truth)
 console.log('This is the JavaScript entry file - your code begins here.');
